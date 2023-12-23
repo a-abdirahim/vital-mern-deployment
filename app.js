41db233efca0bf3773f5086f3d326712dc6b8448
a-abdirahim <cabtamido55@gmail.com>
2023-12-23 12:32:17 +0300

    first commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(
 );
 
 // set http headers
-app.use(path.join(__dirname, "./client/dist"));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
